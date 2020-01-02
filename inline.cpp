c4328eeaca3b267c21fd43357de2aa71a68ceaa5
Tapendra <ptapendra2017@gmail.com>
2020-01-02 08:12:08 +0545

    added program

--- inline.cpp
+++ inline.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
-inline void display(int);
+inline void display(float);
 int main(){
     int salary;
     cout<<"Enter salary:\t";
     cin>>salary;
-    
+    display(salary);
+    return 0;
+
+}
+void display(float salary){
+    float pay;
+    pay=salary-0.1*salary;
+    cout<<"Payment is: "<<pay;
 }
